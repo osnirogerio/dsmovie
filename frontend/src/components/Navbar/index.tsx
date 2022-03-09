1efcb26b3e91cae99a0bca3201b2c2135ad6cfa7
Osni Rogerio de Oliveira <rogerio39@gmail.com>
2022-03-09 10:53:15 -0300

    Security config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                 <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/osnirogerio</p>
                     </div>
                 </a>
             </div>
